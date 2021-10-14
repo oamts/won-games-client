67a16ec57686f06f5608ec583760224ddc1277ea
Mateus <mateusoliveiraalmeida@gmail.com>
2021-10-14 14:42:31 -0300

    Add renderWithTheme helper

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -4,10 +4,6 @@ import Logo from '.'
 
 describe('<Logo />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    it('should render a white label by default', () => {})
   })
 })
