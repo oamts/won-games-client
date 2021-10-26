c0673944c26313542691be66b2d364a722949450
Mateus <mateusoliveiraalmeida@gmail.com>
2021-10-25 21:00:07 -0300

    Add Menu first tests and accessibility

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,17 +8,17 @@ import Logo from '../Logo'
 const Menu = () => (
   <S.Wrapper>
     <S.IconWrapper>
-      <MenuIcon />
+      <MenuIcon aria-label="Open Menu" />
     </S.IconWrapper>
     <S.LogoWrapper>
       <Logo hideOnMobile />
     </S.LogoWrapper>
     <S.MenuGroup>
       <S.IconWrapper>
-        <SearchIcon />
+        <SearchIcon aria-label="Search" />
       </S.IconWrapper>
       <S.IconWrapper>
-        <ShoppingCartIcon />
+        <ShoppingCartIcon aria-label="Open Shopping Cart" />
       </S.IconWrapper>
     </S.MenuGroup>
   </S.Wrapper>
