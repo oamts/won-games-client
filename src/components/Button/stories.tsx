4956f792e99fa766a383775fa018832de99daa85
Mateus <mateusoliveiraalmeida@gmail.com>
2021-11-14 16:38:21 -0300

    Adjust Button visual

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -28,3 +28,12 @@ withIcon.args = {
   children: 'Buy now',
   icon: <AddShoppingCart />
 }
+
+export const asLink: Story = (agrs) => <Button {...agrs} />
+
+asLink.args = {
+  size: 'large',
+  children: 'Buy now',
+  as: 'a',
+  href: '/link'
+}
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -28,10 +28,7 @@ const wrapperModifiers = {
       width: 100%;
     `,
 
-  withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
+  withIcon: (theme: DefaultTheme) => css`  
 
     svg {
       width: 1.5rem;
@@ -45,12 +42,16 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<wrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     cursor: pointer;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
 
     &:hover {
       background: linear-gradient(180deg, #e33565 0%, #d958a6 50%);
